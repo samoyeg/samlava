05ba38d38df8d730f6a0df57cc29fd23ca4b2de4
sam <164807712+samoyeg@users.noreply.github.com>
2024-05-28 16:01:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-43882fdb-f7f4-46a4-876f-40e4caaf57eb/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-161bd747-8075-4daf-ad2a-595bf11335af/)
 
 
 echo "$curl_output" >> Update.tsx
